e315cc8f6b89c636ecf20ef055aebc0aa192d1ae
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-10-17 14:15:23 +0100

    Create nms_agent.py

---
+++ nms_agent.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# import socket
+
+'''
+NMS Agent é responsável por:
+- Comunicação UDP (NetTask)
+- Comunicação TCP (AlertFlow)
+- Execução de Primitivas de Sistema
+    - ping para testar latência
+    - iperf para testes de largura de banda (cliente ou servidor)
+    - Comandos de monitorização de interfaces de rede (ip)
+'''
