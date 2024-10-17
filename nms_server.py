2894915663884db73f0b22ac7eaa713e5d66620c
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-10-17 14:15:21 +0100

    Create nms_server.py

---
+++ nms_server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# import socket
+
+'''
+NMS Server é responsável por:
+- Interpretação de Tarefas
+- Apresentação de Métricas
+- Comunicação UDP (NetTask)
+- Comunicação TCP (AlertFlow)
+- Armazenamento de Dados
+'''
