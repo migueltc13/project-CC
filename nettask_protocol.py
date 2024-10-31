754b34b87c357f21be2b64eb76e41cd168a276af
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-10-31 11:18:51 +0000

    Add NetTask aplicacional protocol functions

---
+++ nettask_protocol.py
@@ -0,0 +1,25 @@
+# Utils module for the server and client
+
+# NetTask Header:
+# - Size of packet (4 bytes)
+# - Sequence number (4 bytes)
+# - Retransmission flag (1 byte)
+# - Message type (1 byte) (0 - Request X, etc.)
+# - Identifier (1 byte)
+# - Message (N bytes)
+
+# Message parser
+def parser(message):
+    size = int(message[:4])
+    seq = int(message[4:8])
+    retransmission = int(message[8])
+    msg_type = int(message[9])
+    return message[10:size + 10], seq, retransmission, msg_type
+
+
+# Message builder
+def build_message(message, seq, retransmission, msg_type):
+    size = len(message)
+    return str(size).zfill(4) + str(seq).zfill(4) + str(retransmission) + str(msg_type) + message
+
+# TODO flow control
