fe2728fa5113b8f418aa6718341beae77949eae1
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-11-02 09:54:05 +0000

    Update nms_agent.py: add client tcp and udp classes and send hello message to server

--- nms_agent.py
+++ nms_agent.py
@@ -11,45 +11,59 @@ NMS Agent é responsável por:
 '''
 
 import socket
-import sys
-import threading
-
-if (len(sys.argv) < 2):
-    ip = '0.0.0.0'
-else:
-    ip = sys.argv[1]
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((ip, 55550))
+import argparse
+import constants as C
 
 
-def recieve():
-    while True:
-        try:
-            message = client.recv(1024).decode('ascii')
-            # parser(message)
-            print(message)
-        except:
-            print("An error occurred!")
-            client.close()
-            break
+class ClientTCP:
+    def __init__(self, server_ip, server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.server_ip, C.TCP_PORT))
+
+    def send_alert(self, alert_message):
+        # TODO format alert message
+        self.client.send(alert_message.encode('ascii'))
+
+    def close(self):
+        self.client.close()
 
 
-def write():
-    while True:
-        message = input()
-        size = len(message)
-        message = str(size) + message
-        client.send(message.encode('ascii'))
+class ClientUDP:
+    def __init__(self, server_ip, server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client.settimeout(1.0)
+
+    def send_hello(self, client_id):
+        # TODO message format
+        self.client.sendto(client_id.encode('ascii'), (self.server_ip, C.UDP_PORT))
 
 
-def parser(message):
-    size = int(message[:4])
-    return message[4:size + 4]
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Network Management System Client")
+    parser.add_argument("--server", "-s", help="Server IP", default="0.0.0.0")
+    args = parser.parse_args()
 
+    server_ip = args.server
+    client_id = socket.gethostname()
 
-recieve_thread = threading.Thread(target=recieve)
-write_thread = threading.Thread(target=write)
+    udp_client = ClientUDP(server_ip, C.UDP_PORT)
+    udp_client.send_hello(client_id)
 
-recieve_thread.start()
-write_thread.start()
+    # TCP - open a connection for sending critical alerts
+    # When running into a crtitical alert situation, send an alert to the server
+
+    # UDP - Send a Hello with identification (hostname)
+    # Await for ACK if not received, retry
+    # Await for tasks from server
+    # Parse/execute tasks in a thread
+
+    # recieve_thread = threading.Thread(target=recieve)
+    # write_thread = threading.Thread(target=write)
+
+    # recieve_thread.start()
+    # write_thread.start()
