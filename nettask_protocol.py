2e46919ed72d392d58040b1bdf0c18bd39e4b4db
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-11-02 09:48:20 +0000

    Update nettask_protocol.py: add ack to message type

--- nettask_protocol.py
+++ nettask_protocol.py
@@ -1,10 +1,12 @@
 # Utils module for the server and client
 
+# TODO use TLV format for messages
+
 # NetTask Header:
 # - Size of packet (4 bytes)
 # - Sequence number (4 bytes)
 # - Retransmission flag (1 byte)
-# - Message type (1 byte) (0 - Request X, etc.)
+# - Message type (1 byte) (0 - Request X, 1 - ACK, etc.)
 # - Identifier (1 byte)
 # - Message (N bytes)
 
@@ -23,3 +25,5 @@ def build_message(message, seq, retransmission, msg_type):
     return str(size).zfill(4) + str(seq).zfill(4) + str(retransmission) + str(msg_type) + message
 
 # TODO flow control
+
+# TODO format of new task
