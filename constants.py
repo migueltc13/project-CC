47319e625a5f4315225353959e1f28a5a0263e06
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-11-15 20:45:42 +0000

    Add data and identifier encoding (utf-8) and protocols versions

--- constants.py
+++ constants.py
@@ -1,3 +1,10 @@
 # Server ports
 TCP_PORT = 5000  # AlertFlow
 UDP_PORT = 6000  # NetTask
+
+# Data and Identifier Encoding
+ENCODING = "utf-8"
+
+# NMS protocols versions
+NET_TASK_VERSION   = 1
+ALERT_FLOW_VERSION = 1
