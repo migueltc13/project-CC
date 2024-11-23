18fd199e5630299f8e7fd00ccc93dc551f61d66d
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-11-23 09:42:42 +0000

    Add server pool, thread manager, locks for sending packets in UDP and update shutdown server functions

--- nms_server.py
+++ nms_server.py
@@ -5,6 +5,7 @@ import threading
 import json
 import os
 import sys
+import time
 import argparse
 import sql.database as db
 import constants as C
@@ -37,6 +38,12 @@ CONFIG_PATH = "config/config.json"
 # SO_NO_CHECK for disabling UDP checksum
 SO_NO_CHECK = 11
 
+# Initial window size for flow control, represents the space available in the server buffer
+INITIAL_WINDOW_SIZE = 64
+
+# Time to sleep before retransmitting packets
+RETRANSMIT_SLEEP_TIME = 5  # seconds
+
 
 ###
 # Configuration
@@ -55,10 +62,11 @@ def load_config(config_path):
 ###
 
 class ServerUI:
-    def __init__(self, server_hostname):
+    def __init__(self, server_hostname, pool):
         self.running = True
         self.view_mode = False  # Real-time view mode
         self.server_hostname = server_hostname
+        self.pool = pool
 
     def display_title(self):
         print("========================================")
@@ -90,7 +98,7 @@ class ServerUI:
         print(f"[WARNING] {message}")
 
     def display_tasks(self, tasks):
-        print("[CONFIGURATION] Loaded Tasks:")
+        print("[CONFIG] Loaded Tasks:")
         print(json.dumps(tasks, indent=2))
 
     def display_menu(self):
@@ -98,7 +106,7 @@ class ServerUI:
         print("Menu")
         print("1. Display Loaded Tasks")
         print("2. View Real-Time Events")
-        print("3. View Connection Status")
+        print("3. View Connected Agents")
         print("4. Shutdown Server")
 
     def handle_menu_choice(self, choice, tcp_server, udp_server, config):
@@ -112,12 +120,21 @@ class ServerUI:
                 input()
                 self.view_mode = False
             case 3:
-                self.display_info("Displaying connection status...")
-                # TODO connection status display using the Pool class
+                agents = self.pool.get_connected_clients()
+                n = len(agents)
+                if n == 0:
+                    print("No agents connected.")
+                else:
+                    print(f"{len(agents)} Connected Agents:")
+                    for agent in agents:
+                        print(f"Agent: {agent}")
+                        # TODO remove the following prints
+                        print(f"Address: {agents[agent]}")
+                        print(f"Sequence Number: {self.pool.get_seq_number(agent)}")
+                        print(f"Packets to Ack: {self.pool.packets_to_ack[agent]}")
+                        print(f"Packets to Reorder: {self.pool.packets_to_reorder[agent]}")
             case 4:
                 self.display_info("Shutting down server...")
-                tcp_server.shutdown()
-                udp_server.shutdown()
                 self.running = False
             case _:
                 self.display_error("Invalid choice. Please try again.")
@@ -144,6 +161,7 @@ class TCPServer(threading.Thread):
         self.server_socket.settimeout(1.0)
         self.shutdown_flag = threading.Event()
         self.alert_flow = AlertFlow(C)
+        self.threads = []
 
         # Start the TCP server
         try:
@@ -158,11 +176,15 @@ class TCPServer(threading.Thread):
         while not self.shutdown_flag.is_set():
             try:
                 client_socket, addr = self.server_socket.accept()
+                client_socket.settimeout(1.0)
 
                 self.ui.save_status(self.server_hostname, f"TCP connection received from {addr}")
 
                 # Create a thread to handle the client socket
-                threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+                handle_client_thread = threading.Thread(target=self.handle_client,
+                                                        args=(client_socket,))
+                self.threads.append(handle_client_thread)
+                handle_client_thread.start()
             except socket.timeout:
                 pass
             except OSError:  # TODO check this exception
@@ -192,13 +214,24 @@ class TCPServer(threading.Thread):
                     pass
 
     def shutdown(self):
+        # Signal all threads to stop
         self.shutdown_flag.set()
-        self.server_socket.close()
+
+        # Wait for all threads to finish
+        for thread in self.threads:
+            if thread.is_alive():
+                thread.join()
+
+        # Close the socket
+        try:
+            self.server_socket.close()
+        except OSError as e:
+            self.ui.display_error(f"Error closing TCP socket: {e}")
 
 
 # UDP Server for NetTask
 class UDPServer(threading.Thread):
-    def __init__(self, ui, host='0.0.0.0', port=C.UDP_PORT):
+    def __init__(self, ui, pool, host='0.0.0.0', port=C.UDP_PORT):
         super().__init__(daemon=True)
         self.ui = ui
         self.host = host
@@ -208,6 +241,9 @@ class UDPServer(threading.Thread):
         self.server_socket.settimeout(1.0)
         self.shutdown_flag = threading.Event()
         self.net_task = NetTask(C)
+        self.pool = pool
+        self.lock = threading.Lock()
+        self.threads = []
 
         # Start the UDP server
         try:
@@ -219,6 +255,31 @@ class UDPServer(threading.Thread):
             sys.exit(1)
         self.ui.save_status(self.server_hostname, f"UDP Server started on port {self.port}")
 
+        # Retransmit packets to be acknowledged
+        ret_thread = threading.Thread(target=self.retransmit_packets)
+        self.threads.append(ret_thread)
+        ret_thread.start()
+
+    def retransmit_packets(self):
+        while not self.shutdown_flag.is_set():
+            # Sleep for a while before retransmitting the packets
+            time.sleep(RETRANSMIT_SLEEP_TIME)
+            if self.shutdown_flag.is_set():
+                break
+
+            # Get the connected agents and respective addresses
+            agents = self.pool.get_connected_clients()
+
+            # For each agent, get the packets to retransmit and retransmit them
+            for agent in agents:
+                addr = agents[agent]
+                packets = self.pool.get_packets_to_ack(agent)
+                # if len(packets) != 0:
+                #     print(f"Retransmitting {len(packets)} packets to {agent}")
+                with self.lock:
+                    for packet in packets:
+                        self.server_socket.sendto(packet, addr)
+
     def run(self):
         while not self.shutdown_flag.is_set():
             try:
@@ -227,7 +288,10 @@ class UDPServer(threading.Thread):
                     break
 
                 # Create a thread to handle the packet
-                threading.Thread(target=self.handle_packet, args=(raw_data, addr,)).start()
+                handle_packet_thread = threading.Thread(target=self.handle_packet,
+                                                        args=(raw_data, addr,))
+                self.threads.append(handle_packet_thread)
+                handle_packet_thread.start()
             except socket.timeout:
                 continue
             except OSError:  # TODO check this exception
@@ -249,35 +313,180 @@ class UDPServer(threading.Thread):
             self.ui.display_error(e)
             return
 
-        # TODO If the packet received is a ACK, process the previous packet sent
+        agent_id = packet["identifier"]
+
+        # If the packet received is a ACK, process the previous packet sent
         # as acknowledged and remove it from the list of packets to be "acked",
         # for that specific agent. After that we interrupt this function.
+        if packet["flags"]["ack"] == 1:
+            self.pool.remove_packet_to_ack(agent_id, packet)
+            return
 
-        # TODO packet reordering and defragmentation
+        # TODO respond to window probes
+
+        # Packet reordering and defragmentation
+        # if the more_flags is set, add the packet to the list of packets to be reordered
+        # else reorder the packets and defragment the data and combine the packets into one
+        if packet["flags"]["more_fragments"] == 1:
+            self.pool.add_packet_to_reorder(agent_id, packet)
+            return
+        else:
+            packet = self.pool.reorder_packets(agent_id, packet)
+
         # TODO flux control by checking the window size
         # if the URG flag is set, send the packet immediately, regardless of the window size
         # if the window size received is 0, send WINDOW_PROBE until the window size updates
+        if packet["flags"]["urgent"] == 1:
+            pass
+        elif self.pool.get_window_size() == 0:
+            # TODO send window probes until the window size updates
+            pass
 
-        # TODO check for packet type
+        # Based on the packet type:
         # - First connection: add the client to the clients pool
         # - Task Metric: save the metric, after parsing data (also save the agent hostname)
         # - End of connection: remove the client from the clients pool
+        match packet["msg_type"]:
+            case self.net_task.FIRST_CONNECTION:
+                self.pool.add_client(agent_id, addr)
+            case self.net_task.SEND_METRICS:
+                # self.ui.save_metric(agent_id, f"Metric data: {packet['data']}")
+                print("TODO save metric")  # TODO
+            case self.net_task.EOC:
+                self.pool.remove_client(agent_id)
 
         # TODO remove this (debug only)
         print(f"Received packet: {json.dumps(packet, indent=2)}")
         # self.ui.save_metric(packet["identifier"], f"Metric data: {packet['data']}")
 
+        # Increment the sequence number for the agent
+        # TODO check if the sequence number is correct between agent and server
+        self.pool.increment_seq_number(agent_id)
+
         # Send ACK
-        seq_number = 0  # TODO
+        seq_number = self.pool.get_seq_number(agent_id)
         identifier = self.server_hostname
-        window_size = 128  # TODO
+        window_size = self.pool.get_window_size()
         ack_packet = self.net_task.build_ack_packet(packet, seq_number,
                                                     identifier, window_size)
-        self.server_socket.sendto(ack_packet, addr)
+        with self.lock:
+            self.server_socket.sendto(ack_packet, addr)
 
     def shutdown(self):
+        # Signal all threads to stop
         self.shutdown_flag.set()
-        self.server_socket.close()
+
+        # Close the socket
+        try:
+            self.server_socket.close()
+        except OSError as e:
+            self.ui.display_error(f"Error closing UDP socket: {e}")
+
+        # Wait for all threads to finish
+        for thread in self.threads:
+            if thread.is_alive():
+                thread.join()
+
+
+class ServerPool:
+    # List of connected agents and respective addresses
+    # List of sequence number of each agent
+    # List of packets sent yet to be acknowledged by each agent
+    # List of packets received yet to be reordered and defragmented
+    # Server buffer size (window size)
+    def __init__(self):
+        self.clients = dict()
+        self.seq_numbers = dict()
+        self.packets_to_ack = dict()
+        self.packets_to_reorder = dict()
+        self.window_size = INITIAL_WINDOW_SIZE
+        self.lock = threading.Lock()
+
+    def add_client(self, client, addr):
+        with self.lock:
+            self.clients[client] = addr
+            self.seq_numbers[client] = 1
+            self.packets_to_ack[client] = []
+            self.packets_to_reorder[client] = []
+
+    def remove_client(self, client):
+        with self.lock:
+            del self.clients[client]
+            del self.seq_numbers[client]
+            del self.packets_to_ack[client]
+            del self.packets_to_reorder[client]
+
+    def get_connected_clients(self):
+        with self.lock:
+            return self.clients
+
+    def get_seq_number(self, client):
+        with self.lock:
+            return self.seq_numbers[client]
+
+    def increment_seq_number(self, client):
+        with self.lock:
+            self.seq_numbers[client] += 1
+
+    def add_packet_to_ack(self, client, packet):
+        with self.lock:
+            self.packets_to_ack[client].append(packet)
+
+    def remove_packet_to_ack(self, client, packet):
+        with self.lock:
+            self.packets_to_ack[client].remove(packet)
+
+    def get_packets_to_ack(self, client):
+        with self.lock:
+            return self.packets_to_ack[client]
+
+    def add_packet_to_reorder(self, client, packet):
+        with self.lock:
+            self.packets_to_reorder[client].append(packet)
+            self.window_size -= 1
+
+    def reorder_packets(self, client, packet):
+        with self.lock:
+            try:
+                # Add the packet to the list of packets to reorder
+                self.packets_to_reorder[client].append(packet)
+                self.window_size -= 1
+                packets = self.packets_to_reorder[client]
+            except KeyError:
+                # No packets to reorder to that client, return the packet as is
+                return packet
+
+            # TODO instead of filtering the packets by the msg_type, add a field to the packet
+            # that indicates the message id, so we can filter by that field, and avoid
+            # getting multiple packets with the same msg_type but different metrics, etc.
+
+            # filter only packets with the same message type
+            packets = [f_packet for f_packet in packets
+                       if f_packet["msg_type"] == packet["msg_type"]]
+
+            # reorder packets
+            packets.sort(key=lambda x: x["seq_number"])
+
+            # defragment data
+            # get the header of the first packet set the more fragments flag to 0,
+            # concatenate the data of all packets (TODO: maybe update the size field)
+            packet = packets[0]
+            packet["flags"]["more_fragments"] = 0
+            for i in range(1, len(packets)):
+                packet["data"] += packets[i]["data"]
+
+            # update the window size
+            self.window_size += len(packets)
+
+            # remove the defragmented packets from the list of packets to reorder
+            self.packets_to_reorder[client] = [packet for packet in self.packets_to_reorder[client]
+                                               if packet not in packets]
+
+            return packet
+
+    def get_window_size(self):
+        with self.lock:
+            return self.window_size
 
 
 ###
@@ -296,11 +505,13 @@ def main():
 
     server_hostname = socket.gethostname()
 
-    ui = ServerUI(server_hostname)
+    pool = ServerPool()
+
+    ui = ServerUI(server_hostname, pool)
     ui.display_title()
 
     tcp_server = TCPServer(ui)
-    udp_server = UDPServer(ui)
+    udp_server = UDPServer(ui, pool)
 
     tcp_server.start()
     udp_server.start()
@@ -310,10 +521,15 @@ def main():
     except (KeyboardInterrupt, EOFError):
         ui.display_info("Server interrupted. Shutting down...")
     finally:
+        print("TODO send EOC to connected agents")  # TODO
+        # send EOC to all connected agents
+        # while the packets to ack are not empty, keep waiting for the acks
         tcp_server.shutdown()
         udp_server.shutdown()
         ui.save_status(server_hostname, "Server shutdown complete.")
-        print("TODO send EOC to connected agents")
+        print("Active threads:", threading.enumerate())
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
