ec9ed9240c4e46650f088f512090b1133052291b
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-12-05 14:43:14 +0000

    Update ping function

--- nms_agent/task_tools/link.py
+++ nms_agent/task_tools/link.py
@@ -1,5 +1,5 @@
 import iperf3
-import ping3
+import subprocess
 
 
 # Iperf
@@ -41,6 +41,38 @@ def iperf3_server(port):
     server.run()
 
 
-# ping: returns the latency in ms
-def ping(host):
-    return ping3.ping(host)
+# Usage:
+# ping("google.com", ["jitter", "packet_lost", "latency"], packet_count=5)
+# Options:
+# - jitter
+# - packet_lost
+# - latency
+def ping(host, options, packet_count=10):
+    command = f"ping -c {packet_count} {host} | tail -n 2"
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if process.returncode != 0:
+        return None
+
+    output = process.stdout.strip()
+
+    # Strip output into two lines
+    lines = output.split("\n")
+    packet_line = lines[0]
+    rtt_line = lines[1]
+
+    # Get the values from the output
+    rtt_values = rtt_line.split("=")[1].strip().split(" ")[0].split("/")
+    packet_loss = packet_line.split(",")[2].strip().split(" ")[0].split("%")[0]
+
+    results = dict()
+    # For each option, add the result to the dictionary
+    for option in options:
+        match option:
+            case "jitter":
+                results[option] = float(rtt_values[3])
+            case "packet_loss":
+                results[option] = float(packet_loss)
+            case "latency":
+                results[option] = float(rtt_values[1])
+
+    return results
