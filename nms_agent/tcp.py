f350c2711441d3b11282cdc727d636c0d3fe3e24
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-12-05 19:10:39 +0000

    Add send lock

--- nms_agent/tcp.py
+++ nms_agent/tcp.py
@@ -1,13 +1,10 @@
 import socket
 import sys
+import threading
 
 import constants as C
 
 from protocol.alert_flow import AlertFlow
-
-# AlertFlow exceptions
-# from protocol.exceptions.invalid_version import InvalidVersionException
-# from protocol.exceptions.invalid_header  import InvalidHeaderException
 
 
 class TCP:
@@ -16,6 +13,7 @@ class TCP:
         self.server_ip = server_ip
         self.server_port = C.TCP_PORT
         self.alert_flow = AlertFlow()
+        self.lock = threading.Lock()
 
         # Check TCP connectivity with the server on initialization
         try:
@@ -32,15 +30,16 @@ class TCP:
         # Build the packet
         packet = self.alert_flow.build_packet(self.alert_flow, self.agent_id, data)
 
-        # Connect to the server
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.server_ip, C.TCP_PORT))
+        with self.lock:
+            # Connect to the server
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.server_ip, C.TCP_PORT))
 
-        # Send the packet
-        self.client_socket.send(packet)
+            # Send the packet
+            self.client_socket.send(packet)
 
-        # Close the connection
-        self.client_socket.close()
+            # Close the connection
+            self.client_socket.close()
 
     def shutdown(self):
         self.client_socket.close()
